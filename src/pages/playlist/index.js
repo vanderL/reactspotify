5ba7e26dce6e501dee54a5c25998668aea84c846
vanderL <vnd.vander@gmail.com>
2021-07-20 13:34:00 -0300

    Playlist Details

--- src/pages/playlist/index.js
+++ src/pages/playlist/index.js
@@ -1,129 +1,128 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Creators as PlaylistDetailsActions } from '../../store/ducks/playlistDetails';
 
 import { Container, Header, SongList } from './styles';
+
+import Loading from '../../components/Loading';
 
 import ClockIcon from '../../assets/images/clock.svg';
 import PlusIcon from '../../assets/images/plus.svg';
 
-const Playlist = () => {
-    return (
-        <Container>
-            <Header>
-                <img 
-                    src="https://i.pinimg.com/originals/8c/d6/b7/8cd6b72ff980245cd55044239c7d1e28.jpg" 
-                    alt="Album cover" 
-                />  
-                <div>
-                    <span>PLAYLIST</span>
-                    <h1>Tyler, the create - New album</h1>
-                    <p>7 músicas</p>
+class Playlist extends Component {
+    static propTypes = {
+        match: PropTypes.shape({
+            params: PropTypes.shape({
+                id: PropTypes.number,
+            })
+        }).isRequired,
+        getPlaylistDetailsRequest: PropTypes.func.isRequired,
+        playlistDetails: PropTypes.shape({
+            data: PropTypes.shape({
+                thumbnail: PropTypes.string,
+                title: PropTypes.string,
+                description: PropTypes.string,
+                song: PropTypes.arrayOf(PropTypes.shape({
+                    id: PropTypes.number,
+                    title: PropTypes.string,
+                    author: PropTypes.string,
+                    album: PropTypes.string,
+                })),
+            }),
+        }).isRequired,
+    }
 
-                    <button>PLAY</button>
-                </div>
-            </Header>
+    componentDidMount() {
+        this.loadPlaylistDetails();
+    }
 
-            <SongList cellPadding={0} cellSpacing={0}>
-                <thead>
-                    <th />
-                    <th>Título</th>
-                    <th>Artista</th>
-                    <th>Albúm</th>
-                    <th>
-                        <img 
-                            src={ClockIcon} 
-                            alt="Duração"
-                        />
-                    </th>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
+    componentDidUpdate(prevProps) {
+        if (prevProps.match.params.id !== this.props.match.params.id) {
+            this.loadPlaylistDetails();
+        }
+    }
+
+    loadPlaylistDetails() {
+        const { id } = this. props.match.params;
+
+        this.props.getPlaylistDetailsRequest(id);
+    }
+    
+    renderDetails = () => {
+        const playlist = this.props.playlistDetails.data;
+
+        return (
+            <Container>
+                <Header>
+                    <img 
+                        src={playlist.thumbnail} 
+                        alt={playlist.title} 
+                    />  
+                    <div>
+                        <span>PLAYLIST</span>
+                        <h1>{playlist.title}</h1>
+                        { !!playlist.songs && <p>{playlist.songs.length} músicas </p> }
+    
+                        <button>PLAY</button>
+                    </div>
+                </Header>
+    
+                <SongList cellPadding={0} cellSpacing={0}>
+                    <thead>
+                        <th />
+                        <th>Título</th>
+                        <th>Artista</th>
+                        <th>Albúm</th>
+                        <th>
                             <img 
-                                src={PlusIcon} 
-                                alt="Adicionar"
+                                src={ClockIcon} 
+                                alt="Duração"
                             />
-                        </td>
-                        <td>RUNNING OUT OF TIME</td>
-                        <td>Tyler</td>
-                        <td>Igor</td>
-                        <td>2:57</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img 
-                                src={PlusIcon} 
-                                alt="Adicionar"
-                            />
-                        </td>
-                        <td>RUNNING OUT OF TIME</td>
-                        <td>Tyler</td>
-                        <td>Igor</td>
-                        <td>2:57</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img 
-                                src={PlusIcon} 
-                                alt="Adicionar"
-                            />
-                        </td>
-                        <td>RUNNING OUT OF TIME</td>
-                        <td>Tyler</td>
-                        <td>Igor</td>
-                        <td>2:57</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img 
-                                src={PlusIcon} 
-                                alt="Adicionar"
-                            />
-                        </td>
-                        <td>RUNNING OUT OF TIME</td>
-                        <td>Tyler</td>
-                        <td>Igor</td>
-                        <td>2:57</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img 
-                                src={PlusIcon} 
-                                alt="Adicionar"
-                            />
-                        </td>
-                        <td>RUNNING OUT OF TIME</td>
-                        <td>Tyler</td>
-                        <td>Igor</td>
-                        <td>2:57</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img 
-                                src={PlusIcon} 
-                                alt="Adicionar"
-                            />
-                        </td>
-                        <td>RUNNING OUT OF TIME</td>
-                        <td>Tyler</td>
-                        <td>Igor</td>
-                        <td>2:57</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img 
-                                src={PlusIcon} 
-                                alt="Adicionar"
-                            />
-                        </td>
-                        <td>RUNNING OUT OF TIME</td>
-                        <td>Tyler</td>
-                        <td>Igor</td>
-                        <td>2:57</td>
-                    </tr>
-                </tbody>
-            </SongList>
-        </Container>
-    )
+                        </th>
+                    </thead>
+                    <tbody>
+                        {!playlist.songs ? (
+                           <tr>
+                               <td colSpan={5}>Nenhuma música cadastrada</td>
+                           </tr> 
+                        ) : (
+                            playlist.songs.map(song => (
+                                <tr key={song.id}>
+                                    <td>
+                                        <img 
+                                            src={PlusIcon} 
+                                            alt="Adicionar"
+                                        />
+                                    </td>
+                                    <td>{song.title}</td>
+                                    <td>{song.author}</td>
+                                    <td>{song.album}</td>
+                                    <td>2:57</td>
+                                </tr>
+                            ))
+                        )}
+                       
+                    </tbody>
+                </SongList>
+            </Container>
+        )
+    }
+
+    render(){
+        return this.props.playlistDetails.loading ? (
+            <Container loading> <Loading /></Container>
+        ) : (
+            this.renderDetails()
+        );
+    }
 }
+const mapStateToProps = state => ({
+    playlistDetails: state.playlistDetails,
+});
 
-export default Playlist;
+const mapDispatchToProps = dispatch => bindActionCreators(PlaylistDetailsActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Playlist)
